8d98162569e768988a90e64f29728ce380a52a26
Gaurav <gauravramnani007@gmail.com>
2024-11-16 00:11:48 -0500

    Code cleanup

--- Database.py
+++ Database.py
@@ -7,6 +7,10 @@ import re
 class Database:
     def __init__(self) -> None:
         self.db = {}
+        self.retailer_item_description_regex = "^[\\w\\s\\-&]+$"
+        self.price_regex = "^\\d+\\.\\d{2}$"
+        self.start_time = time(14, 0, 0)
+        self.end_time = time(16, 0, 0)
 
     def validate_receipt(self, receipt):
         """
@@ -18,13 +22,14 @@ class Database:
             boolean indicating if receipt is valid or not
         """
         validation_res = False
-        if re.fullmatch("^[\\w\\s\\-&]+$", receipt.retailer) != None and re.fullmatch("^\\d+\\.\\d{2}$", receipt.total) != None:
+        if re.fullmatch(self.retailer_item_description_regex, receipt.retailer) != None and \
+            re.fullmatch(self.price_regex, receipt.total) != None:
             for item in receipt.items:
-                if not re.fullmatch("^[\\w\\s\\-]+$", item.shortDescription) or not re.fullmatch("^\\d+\\.\\d{2}$", item.price):
+                if not re.fullmatch(self.retailer_item_description_regex, item.shortDescription) or \
+                    not re.fullmatch(self.price_regex, item.price):
                     validation_res = False
         validation_res = True
         return validation_res
-
 
     def ingest_data(self, receipt) -> str:
         """
@@ -82,6 +87,7 @@ class Database:
 
         receipt_points = 0
 
+        # Count alphanumeric characters in the retailer name and add one point for each.
         alnum = 0
         for char in retailer:
             if char.isalnum():
@@ -89,16 +95,21 @@ class Database:
 
         receipt_points += alnum
 
+        # Add 50 points if total is a round dollar amount with 0 cents.
         if total == int(total):
             receipt_points += 50
         
+        # Add 25 points if total is a multiple of 0.25.
         if total % Decimal(0.25) == 0:
             receipt_points += 25
 
+        # Add 5 points for every 2 items.
         num_items = len(items)
         num_pair_items = num_items // 2
         receipt_points += (num_pair_items * 5)
 
+        # Check the length of item description.
+        # If it is a multiple of 3, multiply the price by 0.2 and round up, and add it to the points total.
         for item in items:
             if len(item.shortDescription.strip()) % 3 == 0:
                 item_points = Decimal(item.price) * Decimal(0.2)
@@ -106,13 +117,12 @@ class Database:
 
         day = purchase_date.strftime("%d")
 
+        # If the day of purchase is odd i.e. 1, 3, 5, 7..., add 6 points.
         if int(day) % 2 == 1:
             receipt_points += 6
 
-        start_time = time(14, 0, 0)
-        end_time = time(16, 0, 0)
-
-        if start_time < purchase_time < end_time:
+        # If the time of purchase is between 2:00 PM and 4:00 PM (non-inclusive), add 10 points.
+        if self.start_time < purchase_time < self.end_time:
             receipt_points += 10
 
         return receipt_points
